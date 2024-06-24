fbb4f8c60c3d513ed67577f79e59fa65e6610d4a
Luigi-Ferraro <fluigic@gmail.com>
2024-06-24 21:25:23 +0000

    metric

--- R/get_parallel_umap_embedding.R
+++ R/get_parallel_umap_embedding.R
@@ -7,12 +7,14 @@
 #' @param n_threads number of threads, NULL if single core
 #' @param n_sgd_threads number of thread for stochastic gradient descent, if > 1 it will not be reproducible
 #' @param grain_size minimum amount of work to do on each thread
+#' @param metric distance metric to use with umap
 #' @return a \emph{embedding_size} x n matrix having observations on columns
 #' @export
 
-get_parallel_umap_embedding <- function(matrix, embedding_size, n_neighbors = 15, n_threads = NULL, n_sgd_threads = 0, grain_size = 1) {
+get_parallel_umap_embedding <- function(matrix, embedding_size, n_neighbors = 15, n_threads = NULL, n_sgd_threads = 0, grain_size = 1, metric = 'cosine') {
     embed = uwot::umap(t(matrix), n_components = embedding_size, n_neighbors = n_neighbors,
-                       n_threads = n_threads, n_sgd_threads = n_sgd_threads, grain_size = grain_size)
+                       n_threads = n_threads, n_sgd_threads = n_sgd_threads, grain_size = grain_size,
+                       target_metric = metric)
     embed = t(embed)
     colnames(embed) = colnames(matrix)
     embed
--- R/get_umap_embedding.R
+++ R/get_umap_embedding.R
@@ -3,12 +3,14 @@
 #' @importFrom umap umap.knn umap.defaults
 #' @param matrix a squared n n x n numeric matrix with values in the range 0-1, with observations on columns
 #' @param embedding_size size of the output embedding
+#' @param metric distance metric to use with umap
 #' @return  a \emph{embedding_size} x n matrix having observations on columns
 #' @export
 
-get_umap_embedding <- function(matrix, embedding_size) {
+get_umap_embedding <- function(matrix, embedding_size, metric = 'cosine') {
     config = umap.defaults
     config$n_components = embedding_size
+    config$metric = metric
 
     embed = umap::umap(t(matrix), config = config)
     embed = t(embed$layout)
